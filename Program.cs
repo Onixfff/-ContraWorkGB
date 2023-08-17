8123d418aa732c5115343e2a55f1e949d78aa171
Onixfff <danilnikonov01@mail.ru>
2023-08-17 15:16:53 +0200

    Закончил создание программы

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿string[] array = {"Мир", "Как дела?", "321", "Это", "Привет!"};
+string[] newArray = new string[0];
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        newArray = GrowsAnArray(newArray, 1);
+        newArray[newArray.Length - 1] = array[i];
+    }
+}
+
+System.Console.WriteLine(ConvertToString(array));
+System.Console.WriteLine(ConvertToString(newArray));
+
+string[] GrowsAnArray(string[] array, int add)
+{
+    string[] newArray = new string[array.Length + 1];
+    for(int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+
+    return array = newArray;
+}
+
+string ConvertToString(string[] array)
+{
+    return $"[{String.Join(", ", array)}]";
+}
